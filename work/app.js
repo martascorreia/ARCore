47deb37bd440345f4bab55467960954097773dc5
Marta Correia <48736172+martacorreia29@users.noreply.github.com>
2022-11-27 15:10:23 +0000

    errors

--- work/app.js
+++ work/app.js
@@ -18,12 +18,13 @@
  * show an error.
  */
 (async function() {
-  const isArSessionSupported = navigator.xr && navigator.xr.isSessionSupported && await navigator.xr.isSessionSupported("immersive-ar");
-  if (isArSessionSupported) {
-    document.getElementById("enter-ar").addEventListener("click", window.app.activateXR)
-  } else {
-    onNoXRDevice();
-  }
+    const isArSessionSupported = navigator.xr && navigator.xr.isSessionSupported &&
+        await navigator.xr.isSessionSupported("immersive-ar");
+    if (isArSessionSupported) {
+        document.getElementById("enter-ar").addEventListener("click", window.app.activateXR)
+    } else {
+        onNoXRDevice();
+    }
 })();
 
 /**
@@ -31,87 +32,87 @@
  * and handle rendering on every frame.
  */
 class App {
-  /**
-   * Run when the Start AR button is pressed.
-   */
-  activateXR = async () => {
-    try {
-      // Initialize a WebXR session using "immersive-ar".
-      this.xrSession = /* TODO */;
+    /**
+     * Run when the Start AR button is pressed.
+     */
+    activateXR = async() => {
+        try {
+            // Initialize a WebXR session using "immersive-ar".
+            this.xrSession = await navigator.xr.requestSession("immersive-ar");
 
-      // Create the canvas that will contain our camera's background and our virtual scene.
-      this.createXRCanvas();
+            // Create the canvas that will contain our camera's background and our virtual scene.
+            this.createXRCanvas();
 
-      // With everything set up, start the app.
-      await this.onSessionStarted();
-    } catch(e) {
-      onNoXRDevice();
+            // With everything set up, start the app.
+            await this.onSessionStarted();
+        } catch (e) {
+            onNoXRDevice();
+        }
     }
-  }
 
-  /**
-   * Add a canvas element and initialize a WebGL context that is compatible with WebXR.
-   */
-  createXRCanvas() {
-    this.canvas = document.createElement("canvas");
-    document.body.appendChild(this.canvas);
-    this.gl = this.canvas.getContext("webgl", {xrCompatible: true});
+    /**
+     * Add a canvas element and initialize a WebGL context that is compatible with WebXR.
+     */
+    createXRCanvas() {
+        this.canvas = document.createElement("canvas");
+        document.body.appendChild(this.canvas);
+        this.gl = this.canvas.getContext("webgl", { xrCompatible: true });
 
-    this.xrSession.updateRenderState({
-      baseLayer: new XRWebGLLayer(this.xrSession, this.gl)
-    });
-  }
+        this.xrSession.updateRenderState({
+            baseLayer: new XRWebGLLayer(this.xrSession, this.gl)
+        });
+    }
 
-  /**
-   * Called when the XRSession has begun. Here we set up our three.js
-   * renderer and kick off the render loop.
-   */
-  async onSessionStarted() {
-    // Add the `ar` class to our body, which will hide our 2D components
-    document.body.classList.add('ar');
+    /**
+     * Called when the XRSession has begun. Here we set up our three.js
+     * renderer and kick off the render loop.
+     */
+    async onSessionStarted() {
+        // Add the `ar` class to our body, which will hide our 2D components
+        document.body.classList.add('ar');
 
-    // To help with working with 3D on the web, we'll use three.js.
-    this.setupThreeJs();
+        // To help with working with 3D on the web, we'll use three.js.
+        this.setupThreeJs();
 
-    // Setup an XRReferenceSpace using the "local" coordinate system.
-    this.localReferenceSpace = /* TODO */;
+        // Setup an XRReferenceSpace using the "local" coordinate system.
+        this.localReferenceSpace = await this.xrSession.requestReferenceSpace("local");
 
-    // Start a rendering loop using this.onXRFrame.
-    this.xrSession.requestAnimationFrame(this.onXRFrame);
-  }
+        // Start a rendering loop using this.onXRFrame.
+        this.xrSession.requestAnimationFrame(this.onXRFrame);
+    }
 
-  /**
-   * Called on the XRSession's requestAnimationFrame.
-   * Called with the time and XRPresentationFrame.
-   */
-  onXRFrame = (time, frame) => {
-    /** TODO draw the application */
-  }
+    /**
+     * Called on the XRSession's requestAnimationFrame.
+     * Called with the time and XRPresentationFrame.
+     */
+    onXRFrame = (time, frame) => {
+        /** TODO draw the application */
+    }
 
-  /**
-   * Initialize three.js specific rendering code, including a WebGLRenderer,
-   * a demo scene, and a camera for viewing the 3D content.
-   */
-  setupThreeJs() {
-    // To help with working with 3D on the web, we'll use three.js.
-    // Set up the WebGLRenderer, which handles rendering to our session's base layer.
-    this.renderer = new THREE.WebGLRenderer({
-      alpha: true,
-      preserveDrawingBuffer: true,
-      canvas: this.canvas,
-      context: this.gl
-    });
-    this.renderer.autoClear = false;
+    /**
+     * Initialize three.js specific rendering code, including a WebGLRenderer,
+     * a demo scene, and a camera for viewing the 3D content.
+     */
+    setupThreeJs() {
+        // To help with working with 3D on the web, we'll use three.js.
+        // Set up the WebGLRenderer, which handles rendering to our session's base layer.
+        this.renderer = new THREE.WebGLRenderer({
+            alpha: true,
+            preserveDrawingBuffer: true,
+            canvas: this.canvas,
+            context: this.gl
+        });
+        this.renderer.autoClear = false;
 
-    // Initialize our demo scene.
-    this.scene = DemoUtils.createCubeScene();
+        // Initialize our demo scene.
+        this.scene = DemoUtils.createCubeScene();
 
-    // We'll update the camera matrices directly from API, so
-    // disable matrix auto updates so three.js doesn't attempt
-    // to handle the matrices independently.
-    this.camera = new THREE.PerspectiveCamera();
-    this.camera.matrixAutoUpdate = false;
-  }
+        // We'll update the camera matrices directly from API, so
+        // disable matrix auto updates so three.js doesn't attempt
+        // to handle the matrices independently.
+        this.camera = new THREE.PerspectiveCamera();
+        this.camera.matrixAutoUpdate = false;
+    }
 };
 
 window.app = new App();
