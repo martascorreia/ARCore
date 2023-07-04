823dc181881dac3f5c9d32780683c3e9bbb24b0c
Marta Correia <marta.b.correia@accenture.com>
2023-07-05 00:30:04 +0100

    removing non used code + sondagem 4

--- shared/utils.js
+++ shared/utils.js
@@ -38,6 +38,13 @@ window.gltfLoader.load("https://immersive-web.github.io/webxr-samples/media/gltf
   window.sunflower = gltf.scene;
 });
 
+window.gltfLoader.load("https://github.com/martacorreia29/MonteDosCastelinhosWebAR/blob/RA_AssistedTracking/app/src/resources/sondagem4%20-%20smaller.glb", function(gltf) {
+  const flower = gltf.scene.children.find(c => c.name === 'sondagem4')
+  flower.castShadow = true;
+  window.sondagem4 = gltf.scene;
+});
+
+
 
 window.DemoUtils = {
   /**
